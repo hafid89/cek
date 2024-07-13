641a82d88dcc309abf9eba0510798b1320b78fc7
hafid89 <144980018+hafid89@users.noreply.github.com>
2024-07-13 11:25:18 +0700

    Update script-a.js

--- tes/script-a.js
+++ tes/script-a.js
@@ -1,5 +1,12 @@
 // banner dan sticky navbar
 window.addEventListener("scroll", function () {
-    var nav = document.querySelector("nav");
-    nav.classList.toggle("sticky", window.scrollY > 0);
-  });
+  var nav = document.querySelector("nav");
+  nav.classList.toggle("sticky", window.scrollY > 0);
+});
+
+// hamburger menu
+function toggleMenu() {
+  const ulc = document.querySelector('.ulc');
+  ulc.classList.toggle('active');
+}
+// hamburger menu end
