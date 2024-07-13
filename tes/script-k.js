70e34c95feaae389040a1f1ac7441bef4ee671df
hafid89 <144980018+hafid89@users.noreply.github.com>
2024-07-13 11:25:34 +0700

    Update script-k.js

--- tes/script-k.js
+++ tes/script-k.js
@@ -3,7 +3,12 @@ window.addEventListener("scroll", function () {
     var nav = document.querySelector("nav");
     nav.classList.toggle("sticky", window.scrollY > 0);
   });
-
+// hamburger menu
+function toggleMenu() {
+    const ulc = document.querySelector('.ulc');
+    ulc.classList.toggle('active');
+  }
+  // hamburger menu end
   document.addEventListener('DOMContentLoaded', function() {
     // Animasi Carousel
     let index = 0;
